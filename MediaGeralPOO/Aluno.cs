ee2ff2491f85c04f73556824404b429e11259f4c
ArturChiodiSiqueira <arturchiodi@outlook.com>
2022-06-14 14:47:00 -0300

    finalizacao

--- MediaGeralPOO/Aluno.cs
+++ MediaGeralPOO/Aluno.cs
@@ -8,7 +8,7 @@ namespace MediaGeralPOO
 {
     internal class Aluno
     {
-        public string Nome { get; set; }
+        public string Nome { get; private set; }
         private double[] _notas;
         public double Media { get { return CalcularMedia(); } }
 
